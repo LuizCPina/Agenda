9cb129134046e1135baaf141912cdc7eef1611dd
LuizCPina <luizricopina@hotmail.com>
2025-12-01 13:10:52 -0300

    Update navegacao.js

--- js/navegacao.js
+++ js/navegacao.js
@@ -11,19 +11,19 @@ function handleNavigation(targetPage) {
 if (tasksButton) {
     tasksButton.addEventListener('click', () => {
       
-        handleNavigation('tasks.html?'); 
+        handleNavigation('app/tasks.html?'); 
     });
 }
 
 if (agendaButton) {
     agendaButton.addEventListener('click', () => {
-        handleNavigation('agenda.html?'); 
+        handleNavigation('app/agenda.html?'); 
     });
 }
 
 
 if (configButton) {
     configButton.addEventListener('click', () => {
-        handleNavigation('configuracoes.html?'); 
+        handleNavigation('app/configuracoes.html?'); 
     });
 }
